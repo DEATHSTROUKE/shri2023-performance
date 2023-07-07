3436d9c6229dbabb07c4c74dbd9f36079bdc8102
unknown <andjeybur@gmail.com>
2023-07-07 19:17:13 +0300

    delete font and optimize images

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./reset.css";
-import "./reset.css";
-import "./fonts.css";
+import "./bootstrap.css";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
